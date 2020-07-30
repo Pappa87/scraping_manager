7a87f3813acb5cf5b4b768c74875f9b25c18b8e5
pappa87 <albertxpapp@gmail.com>
2020-07-30 08:04:33 +0200

    minor change

--- docker_config.py
+++ docker_config.py
@@ -76,8 +76,96 @@ seach_params =\
             "search_exp": "elado+haz+xviii-ker",
           },
           {
+            "name": "budapest_xx",
+            "search_exp": "elado+haz+xx-ker",
+          },
+          {
+            "name": "budapest_xxi",
+            "search_exp":  "elado+haz+xxi-ker",
+          },
+          {
+            "name": "budapest_xxii",
+            "search_exp": "elado+haz+xxii-ker",
+          },
+          {
+            "name": "budapest_xxiii",
+            "search_exp": "elado+haz+xxiii-ker"
+          },
+        ]
+
+seach_params_template =\
+        [
+          {
+            "name": "budapest_i",
+            "search_exp": "elado+haz+i-ker"
+          },
+          {
+            "name": "budapest_ii",
+            "search_exp":  "elado+haz+ii-ker"
+          },
+          {
+            "name": "budapest_iii",
+            "search_exp": "elado+haz+iii-ker"
+          },
+          {
+            "name": "budapest_iv",
+            "search_exp": "elado+haz+iv-ker",
+          },
+          {
+            "name": "budapest_v",
+            "search_exp":  "elado+haz+v-ker",
+          },
+          {
+            "name": "budapest_vi",
+            "search_exp": "elado+haz+vi-ker",
+          },
+          {
+            "name": "budapest_vii",
+            "search_exp": "elado+haz+vii-ker",
+          },
+          {
+            "name": "budapest_viii",
+            "search_exp": "elado+haz+viii-ker",
+          },
+          {
             "name": "budapest_ix",
             "search_exp": "elado+haz+ix-ker",
+          },
+          {
+            "name": "budapest_x",
+            "search_exp": "elado+haz+x-ker",
+          },
+          {
+            "name": "budapest_xi",
+            "search_exp": "elado+haz+xi-ker",
+          },
+          {
+            "name": "budapest_xii",
+            "search_exp": "elado+haz+xii-ker",
+          },
+          {
+            "name": "budapest_xiii",
+            "search_exp": "elado+haz+xiii-ker",
+          },
+          {
+            "name": "budapest_xiv",
+            "search_exp": "elado+haz+xiv-ker",
+          },
+          {
+            "name": "budapest_xv",
+            "search_exp": "elado+haz+xv-ker",
+          },
+          {
+            "name": "budapest_xvi",
+            "search_exp": "elado+haz+xvi-ker",
+          },
+          {
+            "name": "budapest_xvii",
+            "search_exp":  "elado+haz+xvii-ker",
+          },
+          {
+            "name": "budapest_xviii",
+            "search_exp": "elado+haz+xviii-ker",
           },
           {
             "name": "budapest_xx",
@@ -92,8 +180,7 @@ seach_params =\
             "search_exp": "elado+haz+xxii-ker",
           },
           {
-            "name": "budapest_xxiii"
-                    "",
+            "name": "budapest_xxiii",
             "search_exp": "elado+haz+xxiii-ker"
           },
         ]
