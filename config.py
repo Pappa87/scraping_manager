7b17130e461ea51e7ed91b41af0387df1d17e38d
pappa87 <albertxpapp@gmail.com>
2020-07-31 13:08:04 +0200

    minor change

--- config.py
+++ config.py
@@ -4,23 +4,91 @@ docker_host = "tcp://192.168.0.70:2375"
 seach_params =\
         [
           {
+            "name": "budapest_i",
+            "search_exp": "elado+lakas+i-ker"
+          },
+          {
+            "name": "budapest_ii",
+            "search_exp":  "elado+lakas+ii-ker"
+          },
+          {
+            "name": "budapest_iii",
+            "search_exp": "elado+lakas+iii-ker"
+          },
+          {
+            "name": "budapest_iv",
+            "search_exp": "elado+lakas+iv-ker",
+          },
+          {
             "name": "budapest_v",
-            "search_exp": "elado+lakas+v-ker"
+            "search_exp":  "elado+lakas+v-ker",
           },
           {
             "name": "budapest_vi",
-            "search_exp":  "elado+lakas+vi-ker"
+            "search_exp": "elado+lakas+vi-ker",
           },
           {
             "name": "budapest_vii",
-            "search_exp": "elado+lakas+vii-ker"
+            "search_exp": "elado+lakas+vii-ker",
           },
           {
             "name": "budapest_viii",
-            "search_exp": "elado+lakas+viii-ker"
+            "search_exp": "elado+lakas+viii-ker",
           },
           {
             "name": "budapest_ix",
-            "search_exp": "elado+lakas+ix-ker"
-          }
+            "search_exp": "elado+lakas+ix-ker",
+          },
+          {
+            "name": "budapest_x",
+            "search_exp": "elado+lakas+x-ker",
+          },
+          {
+            "name": "budapest_xi",
+            "search_exp": "elado+lakas+xi-ker",
+          },
+          {
+            "name": "budapest_xii",
+            "search_exp": "elado+lakas+xii-ker",
+          },
+          {
+            "name": "budapest_xiii",
+            "search_exp": "elado+lakas+xiii-ker",
+          },
+          {
+            "name": "budapest_xiv",
+            "search_exp": "elado+lakas+xiv-ker",
+          },
+          {
+            "name": "budapest_xv",
+            "search_exp": "elado+lakas+xv-ker",
+          },
+          {
+            "name": "budapest_xvi",
+            "search_exp": "elado+lakas+xvi-ker",
+          },
+          {
+            "name": "budapest_xvii",
+            "search_exp":  "elado+lakas+xvii-ker",
+          },
+          {
+            "name": "budapest_xviii",
+            "search_exp": "elado+lakas+xviii-ker",
+          },
+          {
+            "name": "budapest_xx",
+            "search_exp": "elado+lakas+xx-ker",
+          },
+          {
+            "name": "budapest_xxi",
+            "search_exp":  "elado+lakas+xxi-ker",
+          },
+          {
+            "name": "budapest_xxii",
+            "search_exp": "elado+lakas+xxii-ker",
+          },
+          {
+            "name": "budapest_xxiii",
+            "search_exp": "elado+lakas+xxiii-ker"
+          },
         ]
